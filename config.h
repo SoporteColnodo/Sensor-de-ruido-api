de60492ee1de05502c31463357fb230ab5d9d146
Ander <1megaword1@gmail.com>
2024-01-30 15:00:12 -0500

    registro de privacidad

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
-const char* ssid = "Colnodo_ETB";     // Nombre de la red WiFi
-const char* password = "Colnodo-1700.";    // Contraseña de la red WiFi
-const char* hostname = "ESP32DB_004"; // nombre del dispositivo
+const char* ssid = "usuariodered";     // Nombre de la red WiFi
+const char* password = "contrasena";    // Contraseña de la red WiFi
+const char* hostname = "nombre_de_dispositivo"; // nombre del dispositivo
 
 //extern String createcsv[] = {"general.csv", "newpromediooctubre.csv"};//archivos que se crean
 extern String createcsv[] = {"generaldbcolnodo004.csv"};//archivos que se crean
-extern String User = "esp32ruidocolnodo_004@colnodo.apc.org";//nombre del dispositivo en AA
-extern String Password = "V4X4PbA5xG9.";//contraseña del dispositivo en AA
+extern String User = "nombre del dispositivo en AA";//nombre del dispositivo en AA
+extern String Password = "contraseña del dispositivo en AA.";//contraseña del dispositivo en AA
 
 //Si los dispositivos no se sincronizan o no responden este tiempo puede cambiar es decir el delay no es constante debido
 // a que funciona a partir de la sincronización de dispositivos externos
