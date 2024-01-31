4e85edcdb495b76a9b53aef763f0a7f85a7a3272
Ander <1megaword1@gmail.com>
2024-01-31 10:22:57 -0500

    nuevo commit variaciones de if

--- api-rest-rds.ino
+++ api-rest-rds.ino
@@ -1,202 +1,135 @@
+/*
+El proceso de toma de datos en la siguiente aplicación consta de las siguiente etapas:
+1.Cargue de dispositivos en serial2 de ESP32 (respuesta correcta de GPS,  sds011 aire Y SD)
+2.Cada ciclo de toma de información de los dispositivos se evalua para que sean diferentes a O (cero)
+3.Se genera un ciclo especial de 5 segundos 5000 ms para enviar información a la SD
+4.Si la información enviada a SD es correcta se guarda en un archivo distinto cada hora (ver nota)
+5. Si hay un error en la conexión a wifi, se crea un archivo de respaldo para subir la información manualmente
+
+La hora se estima a partir de los ciclos que se programaron de 2 minutos restandolos de la hora actual generada
+por el RTC
+*/
+
 #include "RTC_Utils.hpp"
+#include <TinyGPSPlus.h>
 #include "config.h"
 #include "indicador.h"
 #include "Wifi_Utils.hpp"
-#include "GPS_Utils.hpp"
-#include "Ruido.hpp"
 #include "SD_Utils.hpp"
 #include "API.hpp"
+#include <SoftwareSerial.h>
 
-int maxAttempts = 100;     // Número máximo de intentos antes de abandonar
-int currentAttempts = 0;  // Número de intentos actual
+//variables inicializar librerias y seriales
+HardwareSerial gpsSerial(2);
+SoftwareSerial sdsSerial(25, 26);
+TinyGPSPlus gps;
 
+// variables muestra en pantalla -PMS -SD nombre
+bool mostrarIndicador = true;
+
+double pm25 = 0.0;      // PM25  
+double pm10 = 0.0;      // PM10  
+double latitud = 0.0;   // latitud
+double longitud = 0.0;  // longitud
+
+DateTime fecha;
+  
 void setup() {
-  RTCSet();
-  Serial.begin(9600);
-  Serial2.begin(9600);
-  delay(500);
-  alertsSetup();
-  CreateCSV(createcsv);
-  gpsData();
+RTCSet();
+SDSet();
+InicioLCD();
+lcd.init();
+Serial.begin(9600);
+gpsSerial.begin(9600);
+sdsSerial.begin(9600);
+delay(2000);
+fecha = DS1307_RTC.now();
+uint32_t milisegundosEnHora = fecha.minute() * 60000 + fecha.second() * 1000;
+maxciclos = (hora - milisegundosEnHora) / intervalSD;
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  String decibeles = "";
-  String latitud = "";
-  String longitud = "";
+unsigned long currentMillis = millis();
+generarPM(); // muestra PMS
+generarLatLng(); // muestra latitud longitud
+if (mostrarIndicador) {
+IndicadorGeneral(latitud, longitud, pm25, pm10); 
+}
+mostrarIndicador = true;
+Serial.println(maxciclos);
+if (currentMillis - previousMillisSD >= intervalSD) {
+static int contador = 0; 
+static double promedioPM25 = 0.0;
+static double promedioPM10 = 0.0;
+static String nombreArchivo;
 
-  // Verifica si ha pasado una hora desde el último envío (cada hora)
-  unsigned long startAPI = millis();  // Tiempo de inicio de ruidoData
-  if (currentMillis - previousMillisAPI >= intervalAPI) {
-    // Actualiza el tiempo del último envío
-    previousMillisAPI = currentMillis;
+previousMillisSD = currentMillis;
+DateTime fecha = DS1307_RTC.now();
+Serial.println(maxciclos);
+if (latitud != 0.0 && longitud != 0.0 && pm25 != 0.0 && pm10 != 0.0) {
+nombreArchivo = String(fecha.day()) + "-" + String(fecha.month()) + "-" + String(fecha.year()) +  "-" + String(fecha.hour()) + ".csv";
+crearArchivo(nombreArchivo);
+okcreacionSD();
+guardaInformacionCSV(latitud, longitud, pm25, pm10, fecha, nombreArchivo);
+mostrarIndicador = false;
+okguardarSD();
+contador++;
+if (contador >= maxciclos) {
+promedioPM25 = calcularPromedioPM(nombreArchivo,maxciclos,"pm25");// se lee archivo y calculan promedios
+promedioPM10 = calcularPromedioPM(nombreArchivo,maxciclos,"pm10");// se lee archivo y calculan promedios
+if(ConnectWiFi_STA()&& promedioPM25 > 0.0 && promedioPM10 > 0.0){
+CrearRegistro(latitud, longitud, promedioPM25, promedioPM10, LoginUser(User, Password));
+WiFi.disconnect(true);
+delay(tmg);
+mostrarIndicador = false;
+okApi();//indicador
+sdsSerial.end();
+}else{
+crearArchivo("informacion_manual_aa.csv");
+guardaInformacionCSV(latitud, longitud, promedioPM25, promedioPM10, fecha, "informacion_manual_aa.csv");
+delay(tmg);
+mostrarIndicador = false;
+errApi();
+sdsSerial.end();
+}
+contador = 0;
+}
+} else {
+Serial.println("Error: GPS no proporciona valores válidos para la API.");
+mostrarIndicador = false;
+errGPS();
+currentAttempts++;
+if (currentAttempts >= maxAttempts) {
+Serial.println("Se superó el número máximo de intentos. Reiniciando.");
+reinicioESP();
+while (true) {
+ESP.restart();
+}
+}
+}
+}
+}
 
-    unsigned long startRuidoData = millis();  // Tiempo de inicio de ruidoData
-    decibeles = ruidoData();
-    unsigned long endRuidoData = millis();  // Tiempo de fin de ruidoData
-    Serial.print("Tiempo de inicio de ruidoData: ");
-    Serial.println(startRuidoData);
-    Serial.print("Tiempo de fin de ruidoData: ");
-    Serial.println(endRuidoData);
-
-    unsigned long startReturnLat = millis();  // Tiempo de inicio de returnLat
-    latitud = returnLat();
-    unsigned long endReturnLat = millis();  // Tiempo de fin de returnLat
-    Serial.print("Tiempo de inicio de returnLat: ");
-    Serial.println(startReturnLat);
-    Serial.print("Tiempo de fin de returnLat: ");
-    Serial.println(endReturnLat);
-
-    unsigned long startReturnLng = millis();  // Tiempo de inicio de returnLng
-    longitud = returnLng();
-    unsigned long endReturnLng = millis();  // Tiempo de fin de returnLng
-    Serial.print("Tiempo de inicio de returnLng: ");
-    Serial.println(startReturnLng);
-    Serial.print("Tiempo de fin de returnLng: ");
-    Serial.println(endReturnLng);
-
-    if (decibeles != "0" && latitud != "0.000000" && longitud != "0.000000") {
-      unsigned long startAPIConnect = millis();  // Tiempo de inicio de ConnectWiFi_STA
-      ConnectWiFi_STA();
-      unsigned long endAPIConnect = millis();  // Tiempo de fin de ConnectWiFi_STA
-      Serial.print("Tiempo de inicio de ConnectWiFi_STA: ");
-      Serial.println(startAPIConnect);
-      Serial.print("Tiempo de fin de ConnectWiFi_STA: ");
-      Serial.println(endAPIConnect);
-
-      unsigned long startAPICreate = millis();  // Tiempo de inicio de Create
-      Create(decibeles, latitud, longitud, LoginUser(User, Password));
-      unsigned long endAPICreate = millis();  // Tiempo de fin de Create
-      Serial.print("Tiempo de inicio de Create: ");
-      Serial.println(startAPICreate);
-      Serial.print("Tiempo de fin de Create: ");
-      Serial.println(endAPICreate);
-
-      unsigned long startAPIDisconnect = millis();  // Tiempo de inicio de WiFi.disconnect
-      WiFi.disconnect(true);
-      delay(100);
-      unsigned long endAPIDisconnect = millis();  // Tiempo de fin de WiFi.disconnect
-      Serial.print("Tiempo de inicio de WiFi.disconnect: ");
-      Serial.println(startAPIDisconnect);
-      Serial.print("Tiempo de fin de WiFi.disconnect: ");
-      Serial.println(endAPIDisconnect);
-
-      unsigned long startAPIOkLed = millis();  // Tiempo de inicio de okLed
-      okLed();
-      unsigned long endAPIOkLed = millis();  // Tiempo de fin de okLed
-      Serial.print("Tiempo de inicio de okLed: ");
-      Serial.println(startAPIOkLed);
-      Serial.print("Tiempo de fin de okLed: ");
-      Serial.println(endAPIOkLed);
-    } else {
-      Serial.println("El GPS arroja valores 'INVALID' que no adecuados para el proceso");
-      unsigned long startAPIErrGPS = millis();  // Tiempo de inicio de errGPS
-      errGPS();
-      unsigned long endAPIErrGPS = millis();  // Tiempo de fin de errGPS
-      Serial.print("Tiempo de inicio de errGPS: ");
-      Serial.println(startAPIErrGPS);
-      Serial.print("Tiempo de fin de errGPS: ");
-      Serial.println(endAPIErrGPS);
-
-      // Puedes aumentar el contador de intentos y salir si se supera el máximo
-      currentAttempts++;
-      if (currentAttempts >= maxAttempts) {
-        Serial.println("Se superó el número máximo de intentos. Abandonando.");
-        unsigned long startAPIErrLed = millis();  // Tiempo de inicio de errLed
-        errLed();
-        unsigned long endAPIErrLed = millis();  // Tiempo de fin de errLed
-        Serial.print("Tiempo de inicio de errLed: ");
-        Serial.println(startAPIErrLed);
-        Serial.print("Tiempo de fin de errLed: ");
-        Serial.println(endAPIErrLed);
-        while (true) {
-          ESP.restart();
-          //while (1);
-          //return;
-        }
-      }
-      return;
+void generarLatLng() {
+while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      latitud = gps.location.lat();
+      longitud = gps.location.lng();
+      Serial.println(latitud);
+      Serial.println(longitud);
     }
   }
+}
 
-  // Verifica si ha pasado 10 minutos desde el último envío (cada 10 minutos)
-  unsigned long startSD = millis();  // Tiempo de inicio de calculoGeneralCSV
-  if (currentMillis - previousMillisSD >= intervalSD) {
-    // Actualiza el tiempo del último envío
-    previousMillisSD = currentMillis;
-
-    DateTime fecha = DS1307_RTC.now();          // RTC
-    unsigned long startRuidoDataSD = millis();  // Tiempo de inicio de ruidoData (SD)
-    decibeles = ruidoData();
-    unsigned long endRuidoDataSD = millis();  // Tiempo de fin de ruidoData (SD)
-   Serial.print("Tiempo de inicio de ruidoData (SD): ");
-    Serial.println(startRuidoDataSD);
-    Serial.print("Tiempo de fin de ruidoData (SD): ");
-    Serial.println(endRuidoDataSD);
-
-    unsigned long startReturnLatSD = millis();  // Tiempo de inicio de returnLat (SD)
-    latitud = returnLat();
-    unsigned long endReturnLatSD = millis();  // Tiempo de fin de returnLat (SD)
-    Serial.print("Tiempo de inicio de returnLat (SD): ");
-    Serial.println(startReturnLatSD);
-    Serial.print("Tiempo de fin de returnLat (SD): ");
-    Serial.println(endReturnLatSD);
-
-    unsigned long startReturnLngSD = millis();  // Tiempo de inicio de returnLng (SD)
-    longitud = returnLng();
-    unsigned long endReturnLngSD = millis();  // Tiempo de fin de returnLng (SD)
-    Serial.print("Tiempo de inicio de returnLng (SD): ");
-    Serial.println(startReturnLngSD);
-    Serial.print("Tiempo de fin de returnLng (SD): ");
-    Serial.println(endReturnLngSD);
-
-    if (decibeles != "0" && latitud != "0.000000" && longitud != "0.000000") {
-      unsigned long startSDCalc = millis();  // Tiempo de inicio de calculoGeneralCSV
-      calculoGeneralCSV(decibeles, latitud, longitud, fecha, createcsv[0]);
-      unsigned long endSDCalc = millis();  // Tiempo de fin de calculoGeneralCSV
-      Serial.print("Tiempo de inicio de calculoGeneralCSV: ");
-      Serial.println(startSDCalc);
-      Serial.print("Tiempo de fin de calculoGeneralCSV: ");
-      Serial.println(endSDCalc);
-
-      unsigned long startSDOkLed = millis();  // Tiempo de inicio de okLed (SD)
-      okLed();
-      unsigned long endSDOkLed = millis();  // Tiempo de fin de okLed (SD)
-      Serial.print("Tiempo de inicio de okLed (SD): ");
-      Serial.println(startSDOkLed);
-      Serial.print("Tiempo de fin de okLed (SD): ");
-      Serial.println(endSDOkLed);
-    } else {
-      Serial.println("El GPS arroja valores 'INVALID' que no adecuados para el proceso");
-      unsigned long startSDErrGPS = millis();  // Tiempo de inicio de errGPS (SD)
-      errGPS();
-      unsigned long endSDErrGPS = millis();  // Tiempo de fin de errGPS (SD)
-      Serial.print("Tiempo de inicio de errGPS (SD): ");
-      Serial.println(startSDErrGPS);
-      Serial.print("Tiempo de fin de errGPS (SD): ");
-      Serial.println(endSDErrGPS);
-
-      // Puedes aumentar el contador de intentos y salir si se supera el máximo
-      currentAttempts++;
-      if (currentAttempts >= maxAttempts) {
-        Serial.println("Se superó el número máximo de intentos. Abandonando.");
-        unsigned long startSDErrLed = millis();  // Tiempo de inicio de errLed (SD)
-        errLed();
-        unsigned long endSDErrLed = millis();  // Tiempo de fin de errLed (SD)
-        Serial.print("Tiempo de inicio de errLed (SD): ");
-        Serial.println(startSDErrLed);
-        Serial.print("Tiempo de fin de errLed (SD): ");
-        Serial.println(endSDErrLed);
-        while (true) {
-          //while (1);
-          ESP.restart();
-          //return;
-        }
-      }
-      return;
-    }
-  }
-  // Reinicia el contador de intentos si llegamos aquí
-  currentAttempts = 0;
+void generarPM() {
+static byte buffer[10];
+Serial.print("valor available pm25: ");
+Serial.println(sdsSerial.available());
+while (sdsSerial.available() >= 10) {
+sdsSerial.readBytes(buffer, 10);
+pm25 = ((buffer[3] << 8) | buffer[2]) / 10.0;
+Serial.println(pm25);
+pm10 = ((buffer[5] << 8) | buffer[4]) / 10.0;
+Serial.println(pm10);
+}
 }
